cea1b47463924804d2aa7466d1fb4fb4203df21c
LoreLor <lorenadearmas01@gmail.com>
2021-12-17 18:20:52 -0300

    resolviendo_conflictos

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,16 +1,10 @@
 import { combineReducers } from 'redux';
 import {productsReducer} from './products.reducer'
-<<<<<<< HEAD
 import {ordenReducer} from './orden.reducer'
-
-export default combineReducers({
-   productsReducer,
-   ordenReducer,
-=======
 import {usersReducer} from './users.reducer'
 
 export default combineReducers({
    productsReducer,
+   ordenReducer,
    usersReducer,
->>>>>>> origin/develop-fran
 });
