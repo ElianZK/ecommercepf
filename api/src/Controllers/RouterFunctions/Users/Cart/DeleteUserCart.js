e95c6887ceb3040e4a422d95fc9680b10a7af05e
ElPugna <tomi.pugna@gmail.com>
2021-12-17 13:20:30 -0300

    Delete User's Cart update

--- api/src/Controllers/RouterFunctions/Users/Cart/DeleteUserCart.js
+++ api/src/Controllers/RouterFunctions/Users/Cart/DeleteUserCart.js
@@ -4,15 +4,13 @@ const deleteUserCart = async(req,res,next)=>{
 try {
   const {UserId} = req.params;
   let user = await User.findByPk(UserId);
-  let cart = await Cart.findAll({
+  let cart = await Cart.destroy({
     where:{
       UserId
     }
   });
-  for(product of cart){
-    await product.destroy();
-  }
-  res.status(200).json({user, cart})
+
+  res.status(200).json({user, deleted:Boolean(cart),productsDeleted:cart})
   
 } catch (error) {
   console.log("DELETE /users/cart/:UserId: ", error);
