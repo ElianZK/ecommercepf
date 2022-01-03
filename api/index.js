d7bfb9f9886b0c575e4d960e7afcd99ba2593072
MasterFSC <44105867+MasterFSC@users.noreply.github.com>
2022-01-03 16:07:55 -0600

    reseteando index.js

--- api/index.js
+++ api/index.js
@@ -7,13 +7,16 @@ var start = false;
 
 
 // Syncing all the models at once.
+onst { conn } = require('./src/db.js');
+const { getProducts} =require('./src/Controllers/DbLoading/getProds');
+const {Product, CategoryBrand, Brand, Category} =require('./src/db');
+var start = false;
+
+
+
+// Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
-  if (start) {
-    server.listen(3001,  () => {
-      console.log('%s listening at 3001'); // eslint-disable-line no-console
-    });
-  }else{
-    try{
+  try{
     //[si ya hay elementos en la base de datos no hago fetch a la api
     let aux = await Product.count();
     //! console.log(aux)
@@ -49,7 +52,6 @@ conn.sync({ force: false }).then(async () => {
          //! console.log("category: ", category.toJSON());
          //! console.log("RELATION: ", relation.toJSON());
          //! console.log("REL ID: ", relId)
-         
         //[Agrego al producto el id de la relación.
         let product = await Product.create({
           ...prod.data, idRelation:relId},
@@ -61,11 +63,8 @@ conn.sync({ force: false }).then(async () => {
     server.listen(3001,  () => {
       console.log('%s listening at 3001'); // eslint-disable-line no-console
     });
-    start = true;
 
-    }catch(err){
+  }catch(err){
     console.log("INDEX: ", err)
-    }
   }
-  
 });
